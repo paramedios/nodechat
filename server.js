b813f3716d486307530a00539de0ba5b9f32ed3b
paramedios <gonsebmedina@gmail.com>
2018-04-14 23:42:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -12,6 +13,18 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
+ axios.get('http://s2.netradiofm.com:5898/stats?sid=1&json=1')
+  .then(response => {
+
+	
+ connection.sendUTF('{"id"}');
+})
+  .catch(error => {
+console.log(error);
+ connection.sendUTF('{"yyyy"}');
+});
+
+
 
 io.on('connection', (socket) => {
   console.log('Client connected');
