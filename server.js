d3bee355f827f029406a4bfb8df905305a4ea8fa
paramedios <gonsebmedina@gmail.com>
2018-04-14 23:44:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-const axios = require('axios');
+var axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
